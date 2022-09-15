266603b1ccf50673e1cab0878ab91c597de66458
Joan Bena <78666333+JoanBena@users.noreply.github.com>
2022-09-15 23:37:25 +0200

    Update get-Teleg_3.4.js

--- get-Teleg_3.4.js
+++ get-Teleg_3.4.js
@@ -1,1 +1,30 @@
+const { Client } = require('tdl')
+const { TDLib } = require('tdl-tdlib-addon')
+const tdlib = new TDLib();
+
+// Teleg_3.3 +3547640625
+// "/root/td/tdlib/lib/libtdjson.so" 
+
+const client4 = new Client(new TDLib(), {
+    apiId: 17784015 , // Your api_id
+    apiHash: "f5a0c8ced83c0975b9ccc4066e2ce51e",
+    databaseDirectory: "_td_databaseTeleg3.4",
+    filesDirectory: "_td_filesTeleg3.4",
+  });
+
+client4.on('error', console.error)
+async function chats(){
+  await client4.connectAndLogin()
+
+  const result = await client4.invoke({
+    _: 'getChats',
+    offset_order: '9223372036854775807',
+    offset_chat_id: 0,
+    limit: 100
+  })
+  
+  console.log(result.chat_ids)
+
+}
+chats()
 
